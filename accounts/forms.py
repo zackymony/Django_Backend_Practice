ec9d6c0d777b52dc2882f43cc2fcad8796f73ff2
Zackymony <a839714439@outlook.com>
2023-02-23 17:03:41 +0800

    fix bugs about register and login. found existing Bugs:registration info didnt change according to the selected role.

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,23 +1,36 @@
-
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+from .models import User
 
-class RegistrationForm(UserCreationForm):
+
+class StudentRegistrationForm(UserCreationForm):
     email = forms.EmailField(required=True)
     birthday = forms.DateField(required=True)
     student_number = forms.CharField(required=True)
-    role = forms.ChoiceField(choices=[('teacher', 'Teacher'), ('student', 'Student')], widget=forms.RadioSelect)
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2', 'birthday', 'student_number', 'role']
+        fields = ['username', 'email', 'password1', 'password2', 'birthday', 'student_number']
 
     def save(self, commit=True):
-        user = super(RegistrationForm, self).save(commit=False)
-        user.role = self.cleaned_data['role']
-        user.student_number = self.cleaned_data.get('student_number', '')
+        user = super(StudentRegistrationForm, self).save(commit=False)
+        user.role = 'student'
+        user.student_number = self.cleaned_data['student_number']
         if commit:
             user.save()
         return user
 
+
+class TeacherRegistrationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+    def save(self, commit=True):
+        user = super(TeacherRegistrationForm, self).save(commit=False)
+        user.role = 'teacher'
+        if commit:
+            user.save()
+        return user
--- accounts/views.py
+++ accounts/views.py
@@ -2,24 +2,34 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .forms import  RegistrationForm
-
+from .forms import TeacherRegistrationForm, StudentRegistrationForm
 
 def register(request):
     if request.method == 'POST':
-        form = RegistrationForm(request.POST)
+        if request.POST.get('role') == 'teacher':
+            form = TeacherRegistrationForm(request.POST)
+        else:
+            form = StudentRegistrationForm(request.POST)
         if form.is_valid():
             form.save()
-            messages.success(request, 'Your account has been created! You are now able to log in.')
-            return redirect('login')
-        else:
-            messages.error(request, 'There was a problem creating your account. Please try again.')
-    else:
-        form = RegistrationForm()
 
+            messages.success(request, 'Your account has been created! You are now able to log in')
+            return redirect('account/login')
+        else:
+            for field, errors in form.errors.items():
+                for error in errors:
+                    messages.error(request, f"{field}: {error}")
+    else:
+        if request.GET.get('role') == 'teacher':
+            form = TeacherRegistrationForm()
+        else:
+            form = StudentRegistrationForm()
     return render(request, 'accounts/register.html', {'form': form})
 
 
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 def user_login(request):
     if request.method == 'POST':
@@ -30,7 +40,7 @@ def user_login(request):
             user = authenticate(username=username, password=password)
             if user is not None:
                 login(request, user)
-                return redirect('home')
+                return render(request, 'accounts/login_success.html')
     else:
         login_form = AuthenticationForm()
     return render(request, 'accounts/login.html', {'form': login_form})
